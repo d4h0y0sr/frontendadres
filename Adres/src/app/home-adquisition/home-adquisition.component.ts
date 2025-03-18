814345c84fcf7ce1f5e77ddcb99c1ae4f054cb38
david hoyos <dahoyosr@gmail.com>
2025-03-17 23:32:01 -0500

    problem cors

--- Adres/src/app/home-adquisition/home-adquisition.component.ts
+++ Adres/src/app/home-adquisition/home-adquisition.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AdquisicionService, Adquisicion } from '../services/adquisicion.service';
 @Component({
   selector: 'app-home-adquisition',
   imports: [],
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './home-adquisition.component.css'
 })
 export class HomeAdquisitionComponent {
+  adquisiciones: Adquisicion[] = [];
 
+  constructor(private adquisicionService: AdquisicionService) { }
+
+  ngOnInit(): void {
+    this.getAdquisiciones();
+  }
+
+  getAdquisiciones(): void {
+    this.adquisicionService.getAdquisiciones().subscribe({
+      next: (data) => {
+        this.adquisiciones = data;
+      },
+      error: (error) => {
+        console.error('Error al obtener las adquisiciones:', error);
+      },
+      complete: () => {
+        console.log('Solicitud completada');
+      }
+    });
+  }
 }
---
+++ Adres/src/app/services/adquisicion.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Adquisicion {
+  id: number;
+  presupuesto: number;
+  unidad: string;
+  tipodeBien: string;
+  cantidad: number;
+  valorUitario: number;
+  valorTotal: number;
+  fechaAdquisicion: string;
+  proveedor: string;
+  documentacion: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdquisicionService {
+
+  private apiUrl = 'localhost:3001/api/adquisiciones';
+
+  constructor(private http: HttpClient) { }
+
+  getAdquisiciones(): Observable<Adquisicion[]> {
+    return this.http.get<Adquisicion[]>(this.apiUrl);
+  }
+}
